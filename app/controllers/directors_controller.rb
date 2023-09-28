c10d9b775aa189a950e4f88fbc27ceb230e88b87
jason ho <43012595+JasonHo404@users.noreply.github.com>
2023-09-28 20:03:55 +0000

    Director page completed

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,10 +4,13 @@ class DirectorsController < ApplicationController
   end
 
   def show
+    #fetch director records
     d_id = params.fetch("d_id")
     records = Director.where({id: d_id})
     @director_records = records.at(0)
 
     render({template: "director_templates/details"})
+
+
   end
 end
